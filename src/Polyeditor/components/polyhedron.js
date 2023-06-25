3ed36d39d878d6cf647c4130c161485083b3b996
gyrgir <135530548+gyrgir@users.noreply.github.com>
2023-06-25 19:09:11 +0200

    Create the mesh from polyhedral data

--- src/Polyeditor/components/polyhedron.js
+++ src/Polyeditor/components/polyhedron.js
@@ -1,14 +1,49 @@
-import { BoxGeometry, Mesh, MeshStandardMaterial } from 'three';
+import { BufferGeometry, Float32BufferAttribute, Mesh, MeshNormalMaterial } from 'three';
+
+import { cube } from '../polyhedra/cube'
+import { tetrahedron } from '../polyhedra/tetrahedron'
+import { octahedron } from '../polyhedra/octahedron'
+
+function triangulate({nodes, faces}) {
+    let vertices = []
+    for (const face of faces) {
+        switch (face.length) {
+            case 3:
+                vertices.push(...nodes[face[0]]);
+                vertices.push(...nodes[face[1]]);
+                vertices.push(...nodes[face[2]]);
+                break;
+            case 4:
+                vertices.push(...nodes[face[0]]);
+                vertices.push(...nodes[face[1]]);
+                vertices.push(...nodes[face[2]]);
+                vertices.push(...nodes[face[0]]);
+                vertices.push(...nodes[face[2]]);
+                vertices.push(...nodes[face[3]]);
+                break;
+            default:
+                console.error("Generic case not yet implemented");
+        }
+    }
+
+    return vertices;
+}
 
 function createPolyhedron() {
-    const material = new MeshStandardMaterial({
-        color: "purple",
-    });
-    const geometry = new BoxGeometry(2, 2, 2);
+    //const material = new MeshStandardMaterial({
+    //    color: "purple",
+    //});
+    const material = new MeshNormalMaterial();
+    const geometry = new BufferGeometry();
 
-    const cube = new Mesh(geometry, material);
+    const positions = triangulate(octahedron);
 
-    return cube;
+    geometry.setAttribute('position', new Float32BufferAttribute(positions, 3));
+    geometry.computeVertexNormals();
+
+    const object = new Mesh(geometry, material);
+
+    return object;
 }
 
 export { createPolyhedron }
