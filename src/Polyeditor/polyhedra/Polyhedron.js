9d83d9e0b99be5019cb7a5b5ee6bdfe91d502fb6
gyrgir <135530548+gyrgir@users.noreply.github.com>
2023-07-16 18:23:26 +0200

    Add calculation of faces sharing each node.

--- src/Polyeditor/polyhedra/Polyhedron.js
+++ src/Polyeditor/polyhedra/Polyhedron.js
@@ -2,13 +2,14 @@ import { Vector3 } from 'three';
 
 class Polyhedron {
 
-    constructor({nodes, faces, nodeFaces: None}) {
+    constructor({nodes, faces, nodeFaces = undefined}) {
         this.nodes = []
         for (const coordinates of nodes) {
             this.nodes.push(new Vector3(...coordinates));
         }
 
         this.faces = faces;
+        this.nodeFaces = nodeFaces || this.calculateNeighborFaces();
     }
 
     getCenter(faceIndex) {
@@ -17,6 +18,19 @@ class Polyhedron {
             (previous, current) => { return previous.add(this.nodes[current]); },
             new Vector3(0, 0, 0));
         return center.multiplyScalar(1.0 / face.length);
+    }
+
+    calculateNeighborFaces() {
+        const neighbors = new Array(this.nodes.length)
+        let i = 0;
+        for (const face of this.faces) {
+            for (const nodeIndex of face) {
+                neighbors[nodeIndex] ||= [];
+                neighbors[nodeIndex].push(i);
+            }
+            i += 1;
+        }
+        return neighbors;
     }
 
 }
