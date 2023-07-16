bfdcd1efea0532aeec4437bf39649d83ce983803
gyrgir <135530548+gyrgir@users.noreply.github.com>
2023-07-16 16:26:57 +0200

    Set valid initial value

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ async function main() {
     const polyeditor = new Polyeditor(document.getElementById("scene-container"));
     polyeditor.start();
 
-    document.getElementById("polyhedra-input").addEventListener('change', (event) => {
+    const polyhedraInput = document.getElementById("polyhedra-input");
+    polyeditor.generate(polyhedraInput.value);
+
+    polyhedraInput.addEventListener('change', (event) => {
         console.log(event.target.value)
         polyeditor.generate(event.target.value);
     })
