6158a2268303799823474e7bd4c3afa181fcb3d8
gyrgir <135530548+gyrgir@users.noreply.github.com>
2023-07-17 21:01:47 +0200

    Billboarded node labels

---
+++ src/Polyeditor/components/createLabel.js
@@ -0,0 +1,29 @@
+import { Sprite, SpriteMaterial, Texture } from "three";
+
+function createLabel(text) {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 128;
+    canvas.height = 128;
+    //tx.fillStyle = "white";
+    //tx.rect(0, 0, 128, 128);
+    //tx.fill();
+    ctx.font = '100px sans-serif';
+
+    ctx.fillStyle = 'black';
+    ctx.textAlign = "center";
+    ctx.fillText(text, 64, 100);
+    //ctx.strokeStyle = 'black';
+    //ctx.strokeText(text, 0, 20);
+
+    const texture = new Texture(canvas);
+    texture.needsUpdate = true;
+
+    const material = new SpriteMaterial({map: texture});
+    const sprite = new Sprite(material);
+    sprite.scale.multiplyScalar(0.5);
+
+    return sprite
+}
+
+export { createLabel }
--- src/Polyeditor/components/nodeLabels.js
+++ src/Polyeditor/components/nodeLabels.js
@@ -1,45 +1,16 @@
-import { Mesh, MeshStandardMaterial, Vector3 } from 'three';
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
-
-function onLoad(font) {
-    console.log(font);
-}
-
-function onProgress(xhr) {
-    console.log((xhr.loaded/xhr.total * 100) + '% loaded');
-}
-
-function onError(err) {
-    console.log(err);
-}
+import { createLabel } from "./createLabel";
 
 function addNodeLabels(polyhedron, scene) {
-    const loader = new FontLoader();
-    const material = new MeshStandardMaterial({color: 0x000000});
     const positionScale = 1.2;
-    loader.load(
-        //'assets/fonts/Montserrat_Regular.json',
-        'node_modules/three/examples/fonts/helvetiker_regular.typeface.json',
-        (font) => {
-            let i = 0;
-            for (const node of polyhedron.nodes) {
-                const geometry = new TextGeometry(i.toString(), {
-                    font: font,
-                    size: 0.2,
-                    height: 0.02
-                });
+    let i = 0;
+    for (const node of polyhedron.nodes) {
+        const label = createLabel(i.toString());
+        label.position.copy(node);
+        label.position.multiplyScalar(positionScale);
+        scene.add(label);
 
-                const object = new Mesh(geometry, material);
-                object.position.copy(node);
-                object.position.multiplyScalar(positionScale);
-
-                scene.add(object);
-                i += 1;
-            }
-        });//,
-        //onProgress,
-        //onError);
+        i += 1;
+    }
 }
 
 export { addNodeLabels }
